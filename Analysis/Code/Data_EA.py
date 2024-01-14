cbd1e544bf0bc7eccbbf581834c5788fae06f652
Alexander Schulz <120401861+AlexanderSchulz14@users.noreply.github.com>
2024-01-14 11:11:34 +0100

    Added test data of factors for EA

--- Analysis/Code/Data_EA.py
+++ Analysis/Code/Data_EA.py
@@ -38,3 +38,15 @@ cpi_ea.rename('CPI_EA', inplace=True)
 # Inflation 
 infl_ea = fred.get_series('CPHPTT01EZM659N')
 infl_ea.rename('Infl_EA', inplace=True)
+
+
+# Yields
+os.chdir(r'C:\Users\alexa\Documents\Studium\MSc (WU)\Master Thesis\Analysis\Data')
+factors_ea = pd.read_csv('ECB_data.csv',
+                         usecols=['DATA_TYPE_FM', 'TIME_PERIOD', 'OBS_VALUE'],
+                         index_col=['TIME_PERIOD'],
+                         infer_datetime_format=True)
+factors_ea.index = pd.to_datetime(factors_ea.index)
+
+factors_ea_sub = factors_ea.loc[factors_ea['DATA_TYPE_FM'].isin(['BETA0', 'BETA1', 
+                                                  'BETA2', 'TAU1'])]
